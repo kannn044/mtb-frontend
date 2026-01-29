073da3c0f7e8bea7f15dd10699045c0a23ec116b
Gun Khemnak <gunnn_boy_hand@hotmail.com>
2026-01-29 10:54:51 +0700

    edit cors

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 const API_URL: string = (() => {
 	// Use the environment variable if it's set, otherwise default to localhost for development.
-	const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
+	const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://203.157.84.69:3001";
 	// Remove any trailing slashes to prevent double slashes in API calls.
 	return apiUrl.replace(/\/+$/, "");
 })();
